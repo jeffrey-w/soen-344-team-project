77acd63eb19742c0cb9d9b499826694818083b3a
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2021-02-06 23:48:23 -0500

    abstract symbol lexing; modify getToken

--- src/main/scanner/AbstractScanner.java
+++ src/main/scanner/AbstractScanner.java
@@ -1,16 +1,20 @@
 package main.scanner;
 
+import main.tokens.IToken;
+
 import java.util.Objects;
 
 import static java.lang.Character.isWhitespace;
 
 /**
- * The {@code AbstractScanner} class provides a minimal implementation of the {@code IScanner} interface. Subclasses
- * must define methods for reading numbers and identifiers from a source file.
+ * The {@code AbstractScanner} class provides a minimal implementation of the {@code IScanner} interface. An {@code
+ * AbstractScanner} reads a source file character by character and provides its representation in {@code IToken}s.
+ * Subclasses must define methods for reading symbols, numbers and identifiers from the source file provided to this
+ * {@code AbstractScanner}.
  */
 public abstract class AbstractScanner implements IScanner {
 
-    private int start, current;
+    private final Position position;
     private final String source;
 
     /**
@@ -21,127 +25,88 @@ public abstract class AbstractScanner implements IScanner {
      */
     protected AbstractScanner(String source) {
         this.source = Objects.requireNonNull(source);
+        this.position = new Position();
     }
 
     @Override
-    public Token getToken() {
+    public IToken getToken() {
         skipWhitespace();
-        switch (nextCharacter()) {
-            case '\0':
-                return Token.EOF;
-            case '*':
-                return Token.AST;
-            case '/':
-                return Token.SLASH;
-            case '+':
-                return Token.PLUS;
-            case '-':
-                return Token.MINUS;
-            case '~':
-                return Token.NOT;
-            case '&':
-                return Token.AND;
-            case '=':
-                return Token.EQL;
-            case '#':
-                return Token.NEQ;
-            case '>':
-                if (peekCharacter() == '=') {
-                    nextCharacter();
-                    return Token.GEQ;
-                }
-                return Token.GTR;
-            case '<':
-                if (peekCharacter() == '=') {
-                    nextCharacter();
-                    return Token.LEQ;
-                }
-                return Token.LSS;
-            case '.':
-                return Token.PERIOD;
-            case ',':
-                return Token.COMMA;
-            case ':':
-                if (peekCharacter() == '=') {
-                    nextCharacter();
-                    return Token.BECOMES;
-                }
-                return Token.COLON;
-            case '!':
-                return Token.OP;
-            case '?':
-                return Token.QUERY;
-            case '(':
-                return Token.LPAREN;
-            case ')':
-                return Token.RPAREN;
-            case ';':
-                return Token.SEMICOLON;
-            case '$':
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                return scanNumber();
-            default:
-                return scanIdentifier();
+        position.collapse();
+        if (isSymbol()) {
+            return scanSymbol();
+        } else if (isNumber()) {
+            return scanNumber();
+        } else {
+            return scanIdentifier();
         }
     }
+
+    /**
+     * Determines whether or not the current character in the source file provided to this {@code AbstractScanner} is a
+     * reserved symbol.
+     *
+     * @return {@code true} if the current character is a reserved symbol
+     */
+    protected abstract boolean isSymbol();
+
+    /**
+     * Consumes the next characters in the source file provided to this {@code AbstractScanner} that can be interpreted
+     * as a reserved symbol.
+     *
+     * @return an {@code IToken} corresponding to the reserved symbol represented by the consumed characters
+     */
+    protected abstract IToken scanSymbol();
+
+    /**
+     * Determines whether or not the current character in the source file provided to this {@code AbstractScanner} is a
+     * number.
+     *
+     * @return {@code true} if the current character is a number
+     */
+    protected abstract boolean isNumber();
 
     /**
      * Consumes the next characters in the source file provided to this {@code AbstractScanner} that can be interpreted
      * as a number.
      *
-     * @return a {@link Token#NUMBER number} {@code Token}
+     * @return a number {@code IToken}
      */
-    protected abstract Token scanNumber();
+    protected abstract IToken scanNumber();
 
     /**
      * Consumes the next characters in the source file provided to this {@code AbstractScanner} that can be interpreted
      * as an identifier.
      *
-     * @return the {@code Token} that corresponds to the reserved word scanned, or an {@link Token#IDENT identifier}
-     * {@code Token}
+     * @return an identifier {@code IToken}
      */
-    protected abstract Token scanIdentifier();
+    protected abstract IToken scanIdentifier();
 
     void skipWhitespace() {
         while (isWhitespace(peekCharacter())) {
             nextCharacter();
         }
-        start = current;
     }
 
-    char previousCharacter() {
-        return source.charAt(current - 1);
+    Position getPosition() {
+        return position;
     }
 
     char nextCharacter() {
-        if (current >= source.length()) {
+        if (position.getCurrent() >= source.length()) {
             return '\0';
         }
-        return source.charAt(current++);
+        return source.charAt(position.advance());
     }
 
     char peekCharacter() {
-        if (current >= source.length()) {
+        if (position.getCurrent() >= source.length()) {
             return '\0';
         }
-        return source.charAt(current);
-    }
-
-    void consume() {
-        start++;
+        return source.charAt(position.getCurrent());
     }
 
     String currentLexeme() {
-        return source.substring(start, current);
+        return source.substring(position.getStart(), position.getCurrent());
     }
 
 }
