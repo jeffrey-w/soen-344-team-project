6d79186ad7301145bcbb2365ec5505be0b26dc2d
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2021-02-10 17:29:45 -0500

    remove from project

--- src/main/scanner/UnsupportedLanguageException.java
+++
@@ -1,8 +0,0 @@
-package main.scanner;
-
-public class UnsupportedLanguageException extends Exception
-{
-    public UnsupportedLanguageException(String errorMessage) {
-        super(errorMessage);
-    }
-}
