b45e73051b4e684201fc0edbb15100d8702f1f57
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2021-02-10 19:26:05 -0500

    organize imports

--- src/test/PositionTest.java
+++ src/test/PositionTest.java
@@ -4,7 +4,7 @@ import main.scanner.Position;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class PositionTest {
 
