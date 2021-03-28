03d29a45cba2abf998f77bc5f199793028f92604
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2021-03-28 10:42:45 -0400

    run all programs

--- src/test/ParserDriver.java
+++ src/test/ParserDriver.java
@@ -4,6 +4,7 @@ import main.parser.IParser;
 import main.picl.interpreter.PrettyPrinter;
 import main.picl.parser.Parser;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -21,13 +22,23 @@ public class ParserDriver {
      * @param args the input arguments
      */
     public static void main(String[] args) {
+        File programDirectory = new File("./programs");
+        File[] programs = programDirectory.listFiles();
+        if (programs != null) {
+            for (File program : programs) {
+                run(program.getPath());
+            }
+        }
+    }
+
+    private static void run(String file) {
         try {
-            byte[] bytes = Files.readAllBytes(Paths.get("./programs/Example.mod"));
+            byte[] bytes = Files.readAllBytes(Paths.get(file));
             IParser parser = new Parser(new String(bytes));
             PrettyPrinter printer = new PrettyPrinter(parser.parse());
             printer.print();
         } catch (IOException e) {
-            System.err.println("Unable to open file.");
+            System.err.println("Unable to open '" + file + "'.");
             System.exit(INVALID_ARGUMENT);
         }
     }
