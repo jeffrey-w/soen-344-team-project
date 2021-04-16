4485bb9c197c9759b5e9f952cc180ef6a8c6d636
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2021-04-16 16:33:40 -0400

    implement first draft of repeat, call, and get evaluations

--- src/main/picl/interpreter/CodeGenerator.java
+++ src/main/picl/interpreter/CodeGenerator.java
@@ -2,50 +2,55 @@ package main.picl.interpreter;
 
 import main.parser.Environment;
 import main.parser.IParser;
+import main.parser.IValue;
 import main.picl.interpreter.decl.*;
 import main.picl.interpreter.expr.*;
 import main.picl.interpreter.stmt.*;
+import main.picl.parser.LiteralValue;
+import main.picl.parser.MemoryAddressValue;
 import main.picl.parser.Parser;
 import main.picl.parser.SyntaxTree;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.Objects;
 
 public class CodeGenerator implements IVisitor {
 
     public static void main(String[] args) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get("./programs/Assignments.mod"));
-        IParser parser = new Parser(new String(bytes));
+        IParser<INode> parser = new Parser(new String(bytes));
         CodeGenerator generator = new CodeGenerator((SyntaxTree) parser.parse());
         generator.generate();
     }
 
-    private int address = 12;
     private int line;
-    private Environment globals;
-    private SyntaxTree ast;
-    private Object stackTop;
-    private PrintWriter stream;
+    private int address; // TODO move to Environment
+    private final SyntaxTree ast;
+    private final Environment globals;
+    private IValue stackTop;
+    private final StringBuilder output; // TODO rename this
 
-    public CodeGenerator(SyntaxTree ast) throws FileNotFoundException {
+    public CodeGenerator(SyntaxTree ast) {
+        address = 0xC;
         this.globals = new Environment();
         this.ast = Objects.requireNonNull(ast);
-        stream = new PrintWriter(new FileOutputStream("a.out"));
+        output = new StringBuilder();
     }
 
     public void generate() {
         ast.getHead().accept(this);
-        stream.close();
+        try (PrintStream stream = new PrintStream(new FileOutputStream("a.out"))) {
+            stream.print(output);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void visitModuleDeclaration(final ModuleDecl declaration) {
-        // TODO
         for (IDecl decl : declaration.getDeclarations()) {
             decl.accept(this);
         }
@@ -56,12 +61,11 @@ public class CodeGenerator implements IVisitor {
 
     @Override
     public void visitVariableDeclaration(final VariableDecl declaration) {
-        // TODO
         for (String identifier : declaration) {
             if (declaration.isConst()) {
-                globals.add(identifier, new Environment.EntryInfo(null, declaration.get(identifier)));
+                globals.add(identifier, new LiteralValue(declaration.get(identifier)));
             } else {
-                globals.add(identifier, new Environment.EntryInfo(declaration.getType(), address++));
+                globals.add(identifier, new MemoryAddressValue(address++));
             }
         }
     }
@@ -86,7 +90,10 @@ public class CodeGenerator implements IVisitor {
 
     @Override
     public void visitIfStatement(final IfStmt statement) {
+        for (Map.Entry<IExpr, IStmt> guardedStatement : statement) {
 
+            guardedStatement.getKey().accept(this);
+        }
     }
 
     @Override
@@ -96,7 +103,12 @@ public class CodeGenerator implements IVisitor {
 
     @Override
     public void visitRepeatStatement(final RepeatStmt statement) {
-
+        int start = line;
+        statement.getStatements().accept(this);
+        if (statement.hasGuard()) {
+            statement.getGuard().accept(this);
+        }
+        output.append(line++).append(" GOTO ").append(start).append("\n");
     }
 
     @Override
@@ -106,34 +118,30 @@ public class CodeGenerator implements IVisitor {
 
     @Override
     public void visitExpressionStatement(final ExpressionStmt statement) {
-        // TODO
         statement.getExpression().accept(this);
     }
 
     @Override
     public void visitAssignmentExpression(AssignmentExpr expression) {
-        // TODO
         expression.getRight().accept(this);
-        Object value = null;
-        String mnemonic = null;
-        if (stackTop instanceof Integer && (Integer) stackTop == 0) {
-            expression.getLeft().accept(this);
-            stream.println(line++ + " CLRF " + ((Environment.EntryInfo)stackTop).value);
-            return;
-        } else if (stackTop instanceof Integer) {
-            mnemonic = " MOVLW ";
-            value = stackTop;
-        } else if (stackTop instanceof Environment.EntryInfo){
-            value = ((Environment.EntryInfo) stackTop).value;
-            if (((Environment.EntryInfo) stackTop).type == null) {
-                mnemonic = " MOVLW ";
+        int value;
+        String mnemonic;
+        if (stackTop instanceof LiteralValue) {
+            if (stackTop.getPayload() == 0) {
+                expression.getLeft().accept(this);
+                output.append(line++).append(" CLRF  1 ").append(stackTop.getPayload()).append("\n");
+                return;
             } else {
-                mnemonic = " MOVFW ";
+                mnemonic = " MOVLW   ";
+                value = stackTop.getPayload();
             }
+        } else {
+            value = stackTop.getPayload();
+            mnemonic = " MOVFW 0 ";
         }
-        stream.println(line++ + mnemonic + value);
+        output.append(line++).append(mnemonic).append(value).append("\n");
         expression.getLeft().accept(this);
-        stream.println(line++ + " MOVWF " + ((Environment.EntryInfo)stackTop).value);   
+        output.append(line++).append(" MOVWF 1 ").append(stackTop.getPayload()).append("\n");
     }
 
     @Override
@@ -142,27 +150,33 @@ public class CodeGenerator implements IVisitor {
 
     @Override
     public void visitComparisonExpression(ComparisonExpr expression) {
-        
     }
 
     @Override
     public void visitArithmeticExpression(ArithmeticExpr expression) {
-
     }
 
     @Override
     public void visitUnaryExpression(final UnaryExpr expression) {
-
     }
 
     @Override
     public void visitCallExpression(final CallExpr expression) {
-
+        if (expression.hasArgument()) {
+            expression.getArgument().accept(this);
+            output.append(line++).append(" MOVFW 0 ").append(stackTop.getPayload()).append("\n");
+        }
+        expression.getCallee().accept(this);
+        output.append(line++).append(" CALL ").append(stackTop.getPayload()).append("\n");
     }
 
     @Override
     public void visitGetExpression(final GetExpr expression) {
-
+        expression.getLeft().accept(this);
+        int register = stackTop.getPayload();
+        expression.getIndex().accept(this);
+        int bit = stackTop.getPayload();
+        output.append(bit).append(" ").append(register);
     }
 
     @Override
@@ -172,7 +186,7 @@ public class CodeGenerator implements IVisitor {
 
     @Override
     public void visitLiteralExpression(final LiteralExpr expression) {
-        stackTop = expression.getValue();
+        stackTop = new LiteralValue(expression.getValue());
     }
 
 }
