c2c1cf90ee104624476ff447a3885d9749ed7a8b
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2021-04-19 14:48:18 -0400

    add type field

--- src/main/picl/parser/MemoryAddressValue.java
+++ src/main/picl/parser/MemoryAddressValue.java
@@ -1,17 +1,24 @@
 package main.picl.parser;
 
-public final class MemoryAddressValue extends AbstractValue{
+public final class MemoryAddressValue extends AbstractValue {
 
-    public MemoryAddressValue(int payload) {
+    private final Enum<?> type;
+
+    public MemoryAddressValue(int payload, Enum<?> type) {
         super(payload);
+        this.type = type; // TODO validate this
     }
 
     @Override
     int validatePayload(int payload) {
-        if (payload < 0xC || payload >= 0x40) {
+        if (payload < 0x5 || payload >= 0x40) {
             throw new Error(); // TODO need picl error
         }
         return payload;
+    }
+
+    public Enum<?> getType() {
+        return type;
     }
     
 }
